5359410b215803c847c1b0a99983af38338ce6f9
Altay Hodoglugil <altay.hodo@gmail.com>
2024-06-10 22:10:33 -0700

    Add end/reset game functionality

--- src/modules/DOMHandler.js
+++ src/modules/DOMHandler.js
@@ -2,14 +2,14 @@ import Player from './player';
 import Gameboard from './gameboard';
 
 const DOMHandler = (function () {
-  const player = new Player('player');
+  let player = new Player('player');
   player.gameboard.placeShips();
-  const computer = new Player('computer');
+  let computer = new Player('computer');
   computer.gameboard.placeShips();
 
   const createGrids = () => {
     const playerGrid = document.querySelector('.player-grid');
-    playerGrid.innerHTML = ''; // Clear any previous content
+    playerGrid.innerHTML = '';
     player.gameboard.array.forEach((row, rowIndex) => {
       row.forEach((item, colIndex) => {
         const gridItem = document.createElement('div');
@@ -74,6 +74,9 @@ const DOMHandler = (function () {
         }
       }
     });
+    if (player.gameboard.isAllShipsSunk()) {
+      endGame(false);
+    }
   };
 
   const playerTurn = (e) => {
@@ -89,7 +92,35 @@ const DOMHandler = (function () {
     } else {
       square.textContent = 'X';
     }
+    if (computer.gameboard.isAllShipsSunk()) {
+      endGame(true);
+    }
     computerTurn();
+  };
+
+  const endGame = (playerWon) => {
+    const winDisplay = document.querySelector('.win-display');
+    playerWon
+      ? (winDisplay.textContent = 'You won!')
+      : (winDisplay.textContent = 'You Lost!');
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.addEventListener('click', resetGame);
+    winDisplay.appendChild(resetButton);
+    const computerGridItems = document.querySelectorAll(
+      '.computer-grid .grid-item'
+    );
+    computerGridItems.forEach((item) => item.removeEventListener('click', playerTurn));
+  };
+
+  const resetGame = () => {
+    player = new Player('player');
+    player.gameboard.placeShips();
+    computer = new Player('computer');
+    computer.gameboard.placeShips();
+    createGrids();
+    const winDisplay = document.querySelector('.win-display');
+    winDisplay.innerHTML = '';
   };
 
   return { createGrids };
